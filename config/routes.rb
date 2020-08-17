f1e1fd5c428cc7250310bb4c1e60685bc8e5ca45
Eli Block <18074026+eliblocks@users.noreply.github.com>
2020-08-16 20:37:28 -0400

    route to frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/test", to: 'test#index'
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  	!request.xhr? && request.format.html?
+	end
 end
